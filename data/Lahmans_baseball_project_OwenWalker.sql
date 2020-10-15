3483d4aa446f62b07633caf5bd51605f27c311fa
OwenWalker <ocw517@gmail.com>
2020-10-15 11:41:20 -0500

    WIP1OwenWalker

--- data/Lahmans_baseball_project_OwenWalker.sql
+++ data/Lahmans_baseball_project_OwenWalker.sql
@@ -1,2 +1,20 @@
-SELECT *
-FROM allstarfull
+-- 1
+Select Max(yearid), min(yearid)
+From teams; 
+
+--Answer 1871 - 2016
+
+-- 3 
+
+SELECT namefirst, namelast, salary, schoolname
+FROM salaries as s
+INNER JOIN people AS p 
+on s.playerid=p.playerid 
+INNER JOIN collegeplaying as c
+on c.playerid=s.playerid
+INNER JOIN schools as sc
+ON sc.schoolid=c.schoolid
+WHERE schoolname = 'Vanderbilt University'
+ORDER BY salary DESC;
+
+-- Answer David Price, 30 Million
